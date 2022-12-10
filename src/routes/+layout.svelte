9bd5463dd2c817d66fe0232c109fe842958dd36b
simondoesstuff <sw.simonwalker@gmail.com>
2022-12-10 03:31:22 -0700

    custom footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
   import "../app.css";
   import Nav from "./comps/navBar/Nav.svelte";
+  import CustomFooter from "./comps/CustomFooter.svelte";
 
 
   let innerWidth;
-  $: small = innerWidth <= 960;
+  $: small = !innerWidth <= 960;
 </script>
 
 <svelte:window bind:innerWidth />
@@ -12,3 +13,4 @@
 
 <Nav orientation={small ? 'horizontal' : 'vertical'}/>
 <slot />
+<CustomFooter/>
---
+++ src/routes/comps/CustomFooter.svelte
@@ -0,0 +1,9 @@
+
+<footer class="w-full text-center p-5 bg-gulf-blue border-t-2 border-[#ffffff33]">
+    <p>
+        Last updated December, 2022.
+    </p>
+    <p class="brightness-[60%]">
+        &copy; Copyright 2022, Simon Walker
+    </p>
+</footer>
